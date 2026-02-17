abfbb75b5d2acc12b0033d883f56b20e7627bc1d
Sihagjaat <amitsihag504@gmail.com>
2026-02-17 18:17:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ WORKERS = 5
 DB_URI = ""
 DB_NAME = "yato"
 
-FSUBS = [[, True, 10]] # Force Subscription Channels [channel_id, request_enabled, timer_in_minutes]
+FSUBS = [[]] # Force Subscription Channels [channel_id, request_enabled, timer_in_minutes]
 # Database Channel (Primary)
 DB_CHANNEL =    # just put channel id dont add ""
 # Multiple Database Channels (can be set via bot settings)
